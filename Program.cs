7b9dacf546c1d3842464df1e03f34eee9af1a55a
wvwvw5 <e.kononykhin@mail.ru>
2023-11-14 20:43:47 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+ShowPapka("/");
+
+void ShowPapka(string s)
+{
+    while (true)
+        {
+        try
+        {
+
+            Console.Clear();
+            string[] paths = Directory.GetDirectories(s);
+            foreach (string path in paths)
+            {
+                Console.WriteLine(" " + path);
+            }
+
+            string[] paths1 = Directory.GetFiles(s);
+            foreach (string path1 in paths1)
+            {
+                Console.WriteLine(" " + path1);
+            }
+
+
+
+            int pos = strelocki.Show(0, paths.Length - 1 + paths1.Length);
+            if (pos == -1)
+                return;
+            if (pos <= paths.Length)
+                ShowPapka(paths[pos]);
+            if (pos >= paths.Length)
+            {
+                Process.Start(new ProcessStartInfo {FileName= paths1[pos - paths.Length],UseShellExecute = true});
+
+            }
+            {
+
+                DirectoryInfo pathInfo = new DirectoryInfo(paths[pos]);
+                Console.WriteLine($"   {paths[pos]}                    {pathInfo.CreationTime}          {pathInfo.Extension}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
+
---
+++ strelocki.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+internal class strelocki
+{
+    
+    public static int Show(int minStrelochka, int maxStrelochka)
+    {
+
+        int pos = 0;
+        ConsoleKeyInfo key;
+
+        do
+        {
+            Console.SetCursorPosition(0, pos);
+            Console.WriteLine("->");
+
+            key = Console.ReadKey();
+
+            Console.SetCursorPosition(0, pos);
+            Console.WriteLine(" ");
+
+            if (key.Key == ConsoleKey.UpArrow && pos != minStrelochka)
+                pos--;
+            else if (key.Key == ConsoleKey.DownArrow && pos != maxStrelochka)
+                pos++;
+            else if (key.Key == ConsoleKey.Escape )
+                return -1;
+        } while (key.Key != ConsoleKey.Enter);
+
+        return pos;
+    }
+}
